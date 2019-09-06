4babc3c92cb14ced1485c693f26bb97d9395a70d
Jetfighte <54713180+Jetfighte@users.noreply.github.com>
2019-09-06 16:21:03 +0300

    Create guess_the_number.py

---
+++ guess_the_number.py
@@ -0,0 +1,27 @@
+import random
+def is_valid_number(s):
+    if s.isdigit() and 0 < int(s) <= 100:
+        return True
+    else:
+        return False
+def main():
+    num = random.randint(1,100)
+    guessed_num = False
+    guess = input("Guess a number from 1 to 100: ")
+    num_guesses = 0
+    while not guessed_num:
+        if not is_valid_number(guess):
+            guess = str(input("I won't count that one. A number between 1 and 100:"))
+        else:
+            num_guesses += 1
+            guess = int(guess)
+        if guess < num:
+            guess = str(input("Too low. Guess again:"))
+        elif guess > num:
+            guess = str(input("Too high. Guess again:"))
+        else:
+            print("You got it in " + num_guesses + " guesses!")
+            guessed_num = True
+    print("Thanks for playing.")
+if __name__ == '__main__':
+    main()
